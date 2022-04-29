2289a1543380c05c72fe50540df615f3047762bc
Çetin Çelik <cetincelik026@gmail.com>
2022-04-29 22:51:07 +0300

    first commit

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+import counterReducers  from "./counterReducer";
+
+const reducers = combineReducers({
+   counterReducers 
+});
+
+
+
+export default reducers;
